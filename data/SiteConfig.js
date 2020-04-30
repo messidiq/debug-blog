69fc4a9ad1bbab7cf750dfba76ec149b3182b863
siddiq <messidiq27@gmail.com>
2020-05-01 05:09:37 +0700

    First

---
+++ data/SiteConfig.js
@@ -0,0 +1,58 @@
+const config = {
+  siteTitle: 'Muhammad Shiddiq F',
+  siteTitleShort: 'Codesbandit',
+  siteTitleAlt: 'Cdsbndt',
+  siteLogo: '/logos/newlogo-1024.png',
+  siteUrl: 'https://codesbandit.netlify.app',
+  repo: 'https://github.com/messidiq',
+  pathPrefix: '',
+  dateFromFormat: 'YYYY-MM-DD',
+  dateFormat: 'MMMM Do, YYYY',
+  siteDescription:
+    'Muhammad Shiddiq is a life-time learner and a business man',
+  siteRss: '/rss.xml',
+  googleAnalyticsID: 'UA-42068444-1',
+  postDefaultCategoryID: 'Tech',
+  newsletter: 'https://taniarascia.substack.com',
+  newsletterEmbed: 'https://taniarascia.substack.com/embed',
+  userName: 'Shiddiq',
+  userEmail: 'messidiq27@gmail.com',
+  userTwitter: 'codesbandit',
+  menuLinks: [
+    {
+      name: 'Me',
+      link: '/me/',
+    },
+    {
+      name: 'Blog',
+      link: '/blog/',
+    },
+    {
+      name: 'Newsletter',
+      link: '/newsletter/',
+    },
+    {
+      name: '404',
+      link: '/note/',
+    },
+
+  ],
+  themeColor: '#3F80FF', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff',
+}
+
+// Make sure pathPrefix is empty if not needed
+if (config.pathPrefix === '/') {
+  config.pathPrefix = ''
+} else {
+  // Make sure pathPrefix only contains the first forward slash
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, '')}`
+}
+
+// Make sure siteUrl doesn't have an ending forward slash
+if (config.siteUrl.substr(-1) === '/') config.siteUrl = config.siteUrl.slice(0, -1)
+
+// Make sure siteRss has a starting forward slash
+if (config.siteRss && config.siteRss[0] !== '/') config.siteRss = `/${config.siteRss}`
+
+module.exports = config
---
+++ src/pages/index.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import GitHubButton from 'react-github-btn'
+import { graphql, Link } from 'gatsby'
+import Layout from '../layout'
+import PostListing from '../components/PostListing'
+import ProjectListing from '../components/ProjectListing'
+import SimpleListing from '../components/SimpleListing'
+import SEO from '../components/SEO'
+import config from '../../data/SiteConfig'
+import projects from '../../data/projects'
+import speaking from '../../data/speaking'
+import podcasts from '../../data/podcasts'
+import quotes from '../../data/quotes'
+import tania from '../../content/images/tania2020crop.jpg'
+
+export default class Index extends Component {
+  render() {
+    const { data } = this.props
+
+    const latestPostEdges = data.latest.edges
+    const popularPostEdges = data.popular.edges
+
+    return (
+      <Layout>
+        <Helmet title={`${config.siteTitle} – Full Stack Software Developer`} />
+        <SEO />
+        <div className="container">
+          <div className="lead">
+            <div className="elevator">
+              <h1>{`Hai, Saya Muhammad Shiddiq 👋`} </h1>
+              <p>
+                {`I'm a full stack software developer creating android`}
+                projects and <Link to="/blog">writing</Link> about modern JavaScript, Node.js, and
+                development.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="container front-page">
+          <section className="section">
+            <h2>
+              Latest Articles
+              <Link to="/blog" className="view-all">
+                View all
+              </Link>
+            </h2>
+            <PostListing simple postEdges={latestPostEdges} />
+          </section>
+
+          <section className="section">
+            <h2>
+              Most Popular
+              <Link to="/categories/popular" className="view-all">
+                View all
+              </Link>
+            </h2>
+            <PostListing simple postEdges={popularPostEdges} />
+          </section>
+
+          <section className="section">
+            <h2>Open Source Projects</h2>
+            <ProjectListing projects={projects} />
+          </section>
+
+          <section className="section">
+            <h2>Interviews</h2>
+            <SimpleListing simple data={podcasts} />
+          </section>
+
+          <section className="section">
+            <h2>Talks</h2>
+            <SimpleListing simple data={speaking} />
+          </section>
+        </div>
+        <div className="gradient-section">
+          <div className="container">
+            <h2>Other People Say...</h2>
+          </div>
+          <div className="quotations">
+            {quotes.map(quote => (
+              <blockquote className="quotation" key={quote.name}>
+                <p>{quote.quote}</p>
+                <cite>— {quote.name}</cite>
+              </blockquote>
+            ))}
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    latest: allMarkdownRemark(
+      limit: 5
+      sort: { fields: [fields___date], order: DESC }
+      filter: { frontmatter: { template: { eq: "post" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            date
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            categories
+            thumbnail {
+              childImageSharp {
+                fixed(width: 150, height: 150) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+            date
+            template
+          }
+        }
+      }
+    }
+    popular: allMarkdownRemark(
+      limit: 9
+      sort: { fields: [fields___date], order: DESC }
+      filter: { frontmatter: { categories: { eq: "Popular" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            date
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            categories
+            thumbnail {
+              childImageSharp {
+                fixed(width: 150, height: 150) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+            date
+            template
+          }
+        }
+      }
+    }
+  }
+`
